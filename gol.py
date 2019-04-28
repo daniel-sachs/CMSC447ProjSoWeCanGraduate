bcf0748b287e96feee77db0d3914b90d30371040
Innocent Kironji <wambugu1@umbc.edu>
2019-04-27 20:01:04 -0400

    GUI Updates
    - Added code to allow the player stats to be updated as the game runs
    * Becuase of frequency of the calls to update the game is very resource intensive and does not always update the screen on time
    * The slider code does not work

--- gol.py
+++ gol.py
@@ -7,9 +7,9 @@ from threading import Timer
 #-------------------------------------------------------------------
 
 # Speeds up the game_speed based on the value found at the slider
-def SpeedUp(p1, p2, speed = 2):
-    p1.game_speed *= speed
-    p2.game_speed *= speed
+def SetSpeed(p1, p2, speed = 200):
+    p1.game_speed = speed
+    p2.game_speed = speed
 
 def end_of_game(p1, p2):
     t = Timer(1.5, p1.root.quit)
@@ -57,22 +57,16 @@ class Menubar(tk.Frame):
 
 		fileMenu = tk.Menu(menubar)
 		fileMenu.add_command(label="New", command=self.onNew)
-		fileMenu.add_command(label="View Scores", command=self.onView)
+		#fileMenu.add_command(label="View Scores", command=self.onView)
 		fileMenu.add_command(label="Exit", command=self.onExit)
 		menubar.add_cascade(label="File", menu=fileMenu)
 
 		generalMenu = tk.Menu(menubar)
-		generalMenu.add_command(label="Start", command=self.onEasy)
-		generalMenu.add_command(label="Stop", command=self.onHard)
-		generalMenu.add_command(label="Set Turns", command=self.onDefault)
-		generalMenu.add_command(label="Reset", command=self.onDefault)
+		generalMenu.add_command(label="Start", command=self.onStub)
+		generalMenu.add_command(label="Stop", command=self.onStub)
+		generalMenu.add_command(label="Set Turns", command=self.onStub)
+		generalMenu.add_command(label="Reset", command=self.onStub)
 		menubar.add_cascade(label="Game", menu=generalMenu)
-
-		controlMenu = tk.Menu(menubar)
-		controlMenu.add_command(label="Speed Up", command=SpeedUp)
-		controlMenu.add_command(label="Edit Color", command=self.onHard)
-		controlMenu.add_command(label="Edit Tiles", command=self.onDefault)
-		menubar.add_cascade(label="Controls", menu=controlMenu)
 
 		helpMenu = tk.Menu(menubar)
 		helpMenu.add_command(label="General", command=self.onGeneral)
@@ -99,17 +93,8 @@ class Menubar(tk.Frame):
 	def onExit(self):
 		self.quit()
 
-	def onEasy(self):
+	def onStub(self):
 		print()
-
-	def onHard(self):
-		print()
-
-	def onDefault(self):
-		global happiness_step
-		global hunger_step
-		hunger_step = 1
-		happiness_step = 2
 
 	def onGeneral(self):
 		master = tk.Tk()
@@ -130,18 +115,12 @@ class Menubar(tk.Frame):
 		master.title("Menubar Help")
 		info = "There are three main menus in this game (with various sub-options).\n\n \
 The first is the file menu. This menu has three options 'new', 'view scores' and 'exit'  \n\n \
-The 'new' option allows you to start a new game. This can be used when you get a gameover or simplly to reset during the middle of a current game \n\n \
+The 'new' option allows you to start a new game. This can be used when you get a game over or simpley to reset during the middle of a current game \n\n \
 The 'view scores' option allows the user to view previous scores they have achieved in the game. The scores show up near the bottom corner of the main screen and will disappear after about 15 seconds.\n\n \
-The 'exit' option closes the entire game. This may not close mini-games but they should all automatically close after some time \n\n\n \
-The second menu is the game menu. This menu has three different options and helps control the pace/ difficulty of the game. \n\n \
-The first option is 'slow down' which slows down (by half) the rate at which Lucifer the cat becomes unhappy with you.\n\n \
-Be careful because this option has no effect on hunger \n \
-The second option is 'speed up' which doubles the rate at which Lucifer becomes hungry and becomes bored. \n\n \
-This can make the game very challenging because it does not speed up the cycle of the days \n \
-The third option is 'reset' which resets Lucifer's rate of hunger and boredom to their default values \n\n \
+The 'exit' option closes the entire game. \n\n\n \
+The second menu is the game menu. \n\n \
 The third menu, the help menu, is the one you used to get here! It has 5 differnt options \n\n\n \
 This is the menu where you can find the instructions for the game. There are instructions for how to play the game in general \n\n \
-There are also instructions for how to play the various mini-games as well as this set of instructions which explains the menu system \n\n \
 	Exit this help menu by pressing 'quit' to the right of this text"
 		S = tk.Scrollbar(master)
 		T = tk.Text(master, height=20, width=50)
@@ -156,142 +135,153 @@ There are also instructions for how to play the various mini-games as well as th
 #---------------------------------------------------------------------
 
 class Game:
-
-    def __init__(self, canvas, root, pFrame, gameSpeed, my_turn, color = 'forest green', dead_color = 'green2', adversary = None):
-        self.canvas = canvas
-        self.root = root
-        self.stats_frame = pFrame
-        self.grid = [] # Variable to store the Cell objects
-        self.rectangles = [] # Variable to store self.rectangles
-        self.begin_id = None
-        self.game_speed = gameSpeed
-        self.color = color
-        self.dead_color = dead_color
-        self.create_grid()
-        self.total_alive = 0
-        self.total_dead = 0
-        self.cells_left = 15
-        self.canvas.bind("<Button-1>", self.change_colour_on_click)
-        self.adversary = adversary
-        self.my_turn = my_turn
-        self.original_turn = my_turn
+	
+	def __init__(self, canvas, root, pFrame, gameSpeed, my_turn, color = 'forest green', dead_color = 'green2', adversary = None):
+		self.canvas = canvas
+		self.root = root
+		self.stats_frame = pFrame
+		self.grid = [] # Variable to store the Cell objects
+		self.rectangles = [] # Variable to store self.rectangles
+		self.begin_id = None
+		self.game_speed = gameSpeed
+		self.color = color
+		self.dead_color = dead_color
+		self.board_len = 35
+		self.board_size = self.board_len ** 2
+		self.create_grid()
+		self.total_alive = 0
+		self.total_dead = 0
+		self.cells_left = 15
+		self.canvas.bind("<Button-1>", self.change_colour_on_click)
+		self.adversary = adversary
+		self.my_turn = my_turn
+		self.original_turn = my_turn
 
 
     # Function for updating the values for the player stats
-    def updateFrame(self):
-        alive = 0
-        dead = 1
-        self.stats_frame[alive].config(text = "Total Alive: " + str(self.total_alive))
-        self.stats_frame[dead].config(text = "Total Dead: " + str(self.total_dead))
+	def updateFrame(self):
+		cellToChange = 0
+		remaining = 1
+		alive = 2
+		dead = 3
+		speed_slider = 4
+		self.stats_frame[cellToChange].config(text = "Cells to Change: " + str(self.cells_left))
+		num_white = self.board_size - self.total_alive
+		self.stats_frame[remaining].config(text = "Remaining White: %.2f" % (num_white / self.board_size * 100) + "%")
+		self.stats_frame[alive].config(text = "Score: " + str(self.total_alive))
+		self.stats_frame[dead].config(text = "Dead Cells: " + str(self.total_dead))
+		#self.game_speed = self.stats_frame[speed_slider].get() # This line updates the game speed
 
     # This function creates the board on which the game will take place
-    def create_grid(self):
-        x = 10
-        y = 10
-        for i in range(35): #height
-            self.grid.append([])
-            self.rectangles.append([])
-            for j in range(35): #width
-                rect = self.canvas.create_rectangle(x, y, x+10, y+10, fill="white")
-                self.rectangles[i].append(rect)
-                self.grid[i].append(Cell(x, y, i, j))
-                x += 10
-            x = 10
-            y += 10
+	def create_grid(self):
+		x = 10
+		y = 10
+		for i in range(self.board_len): #height
+			self.grid.append([])
+			self.rectangles.append([])
+			for j in range(self.board_len): #width
+				rect = self.canvas.create_rectangle(x, y, x+10, y+10, fill="white")
+				self.rectangles[i].append(rect)
+				self.grid[i].append(Cell(x, y, i, j))
+				x += 10
+			x = 10
+			y += 10
 
 
     # Find the co-ordinates of the rectangle which has been clicked
-    def find_rect_coordinates(self, x, y):
-        return (x- x%10, y - y%10)
+	def find_rect_coordinates(self, x, y):
+		return (x- x%10, y - y%10)
 
 
     # Change the colour of the clicked self.grid and change the status of cell in the self.grid
-    def change_colour_on_click(self, event):
-        if self.cells_left == 0:
-            print("no cells left and player 1 turn over")
-            self.my_turn = False
+	def change_colour_on_click(self, event):
+		if self.cells_left == 0:
+			print("no cells left and player 1 turn over")
+			self.my_turn = False
             
-        elif self.cells_left > 0 and self.adversary.my_turn != True:
-            print(event.x, event.y)
-            x, y = self.find_rect_coordinates(event.x, event.y)
-            try:
-                iy = int(x / 10 - 1)
-                ix = int(y / 10 - 1)
-                if ix == -1 or iy == -1:
-                    raise IndexError
-                if self.grid[ix][iy].isAlive:
-                    self.canvas.itemconfig(self.rectangles[ix][iy], fill=self.dead_color)
-                    self.total_dead += 1
-                else:
-                    self.canvas.itemconfig(self.rectangles[ix][iy], fill=self.color)
-                    self.total_alive += 1
-                self.grid[ix][iy].switchStatus()
-                print(self.grid[ix][iy].pos_matrix, self.grid[ix][iy].pos_screen)
-                self.updateFrame()
-            except IndexError:
-                return
-            print("clicked")
-            self.cells_left = self.cells_left - 1
-            print("one less cell now")
+		elif self.cells_left > 0 and self.adversary.my_turn != True:
+			print(event.x, event.y)
+			x, y = self.find_rect_coordinates(event.x, event.y)
+			try:
+				iy = int(x / 10 - 1)
+				ix = int(y / 10 - 1)
+				if ix == -1 or iy == -1:
+					raise IndexError
+				if self.grid[ix][iy].isAlive:
+					self.canvas.itemconfig(self.rectangles[ix][iy], fill=self.dead_color)
+					self.total_dead += 1
+				else:
+					self.canvas.itemconfig(self.rectangles[ix][iy], fill=self.color)
+					self.total_alive += 1
+				self.grid[ix][iy].switchStatus()
+				print(self.grid[ix][iy].pos_matrix, self.grid[ix][iy].pos_screen)
+				self.updateFrame()
+			except IndexError:
+				return
+			print("clicked")
+			self.cells_left = self.cells_left - 1
+			self.updateFrame()
+			print("one less cell now")
 
 
-    def paint_grid(self):
-        for i in self.grid:
-            for j in i:
-                if j.nextStatus != j.isAlive:
-                    x, y = j.pos_matrix
-                    print(x, y)
-                    if j.nextStatus:
-                        self.canvas.itemconfig(self.rectangles[x][y], fill=self.color)
-                        print("changed", j.pos_matrix, "from dead to alive")
-                        self.total_alive += 1
-                    else:
-                        self.canvas.itemconfig(self.rectangles[x][y], fill=self.dead_color)
-                        self.total_dead += 1
-                        print("changed", j.pos_matrix, "from alive to dead")
-                    j.switchStatus()
-                    print("Current status of", j.pos_matrix, j.isAlive)
-                self.updateFrame()
-        print("Done painting")
+	def paint_grid(self):
+		for i in self.grid:
+			for j in i:
+				if j.nextStatus != j.isAlive:
+					x, y = j.pos_matrix
+					print(x, y)
+					if j.nextStatus:
+						self.canvas.itemconfig(self.rectangles[x][y], fill=self.color)
+						print("changed", j.pos_matrix, "from dead to alive")
+						self.total_alive += 1
+					else:
+						self.canvas.itemconfig(self.rectangles[x][y], fill=self.dead_color)
+						self.total_dead += 1
+						print("changed", j.pos_matrix, "from alive to dead")
+					j.switchStatus()
+					print("Current status of", j.pos_matrix, j.isAlive)
+				self.updateFrame()
+		print("Done painting")
 
 
-    def changeInStatus(self, cell):
-        ''' If the cell's status changes in the next gen, return True else False '''
-        num_alive = 0
-        x, y = cell.pos_matrix
-        for i in (x-1, x, x+1):
-            for j in (y-1, y, y+1):
-                if i == x and j == y:
-                    continue
-                if i == -1 or j == -1:
-                    continue
-                try:
-                    if self.grid[i][j].isAlive:
-                        num_alive += 1
-                except IndexError:
-                    pass
-        if cell.isAlive:
-            return not( num_alive == 2 or num_alive == 3 )
-        else:
-            return num_alive == 3
+	def changeInStatus(self, cell):
+		''' If the cell's status changes in the next gen, return True else False '''
+		num_alive = 0
+		x, y = cell.pos_matrix
+		for i in (x-1, x, x+1):
+			for j in (y-1, y, y+1):
+				if i == x and j == y:
+					continue
+				if i == -1 or j == -1:
+					continue
+				try:
+					if self.grid[i][j].isAlive:
+						num_alive += 1
+				except IndexError:
+					pass
+		if cell.isAlive:
+			return not( num_alive == 2 or num_alive == 3 )
+		else:
+			return num_alive == 3
 
 
-    def begin(self):
-        for i in self.grid:
-            for j in i:
-                if self.changeInStatus(j):
-                    j.nextStatus = not j.isAlive
-                    print("change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus)
-                else:
-                    j.nextStatus = j.isAlive
+	def begin(self):
+		for i in self.grid:
+			for j in i:
+				if self.changeInStatus(j):
+					j.nextStatus = not j.isAlive
+					print("change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus)
+				else:
+					j.nextStatus = j.isAlive
 
-        self.paint_grid()
-        self.begin_id = self.root.after(self.game_speed, self.begin) # Can be used to change speed
+		self.paint_grid()
+		self.begin_id = self.root.after(self.game_speed, self.begin) # Can be used to change speed
 
 
-    def stop(self):
-        self.cells_left = 15
-        self.my_turn = self.original_turn
-        self.root.after_cancel(self.begin_id)
+	def stop(self):
+		self.cells_left = 15
+		self.updateFrame()
+		self.my_turn = self.original_turn
+		self.root.after_cancel(self.begin_id)
 
 #-------------------------------------------------------------------
