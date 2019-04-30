db2ef5826c992b48d1a1b427bf3276227a429f67
Innocent Kironji <wambugu1@umbc.edu>
2019-04-30 11:12:17 -0400

    Getting Player Info (Beta)
    Adding a function to get information about a player (name and color)

--- main.py
+++ main.py
@@ -5,6 +5,36 @@ from time import sleep
 from gol import Menubar, Game, SetSpeed, end_of_game
 from functools import partial
 
+# Getting color and name info for players
+def getInfo(num_players):
+    info = []
+    for i in range(num_players):
+        master = tk.Tk()
+        master.title("Getting Started")
+        color_prompt = "Please pick a color from one of the following (type the number): \n 1.Green 2.Blue 3.Red 4.Purple"
+        name_prompt = "Player " + str(i + 1) + " enter your name below:"
+        label1 = tk.Label(master, text=color_prompt)
+        label1.grid(row=0)
+        label1.pack()
+        e1 = tk.Entry(master)
+        e1.grid(row=0, column=1)
+        e1.pack()
+
+        label2 = tk.Label(master, text=name_prompt)
+        label2.grid(row=4)
+        label2.pack()
+        e2 = tk.Entry(master)
+        e2.grid(row=4, column=4)
+        e2.pack()
+        
+        exitButton = tk.Button(master, text = "Done", command = master.destroy)
+        exitButton.pack()
+
+        info.append(e1.get())
+        print(e1.get())
+        info.append(e2.get())
+    master.mainloop()
+    return info
 
 def begin_game(turnCounter, iters, turns, speed):
     updateDisplay(turnCounter, iters, turns, speed)
@@ -46,13 +76,24 @@ def main():
     default_speed = max_speed * 2
     min_speed = default_speed + max_speed
     defaultCells = 15
+    p1_color = 0
+    p1_name = "Player 1"
+    p2_color = 1
+    p2_name = "Player 2"
 
     # Player colors, list[0] = alive color, list[1] = dead color
     green = ['forest green','green2']
     blue = ['blue2', 'cyan2']
     red = ['red', 'salmon']
     purple = ['purple1', 'MediumPurple1']
-    colors = [green, blue, red]
+    colors = [green, blue, red, purple]
+
+
+    game_info = getInfo(2)
+    p1_color = (int(game_info[0]) - 1) % 4
+    p1_name = game_info[1]
+    p2_color = (int(game_info[2]) - 1) % 4
+    p2_name = game_info[3]
 
     # Building the screen
     root = tk.Tk()
@@ -99,9 +140,9 @@ def main():
     frame_Banner.pack()
     frame_Banner.place(x=288, y=198)
     #player1Title =
-    tk.Label(frame_Banner, text="Player 1", bg="gray25", fg="snow", font=(None, "16"), width=33).pack(side="left")
+    tk.Label(frame_Banner, text=p1_name, bg="gray25", fg="snow", font=(None, "16"), width=33).pack(side="left")
     #player2Title =
-    tk.Label(frame_Banner, text="Player 2", bg="gray25", fg="snow", font=(None, "16"), width=33).pack(side="right")
+    tk.Label(frame_Banner, text=p2_name, bg="gray25", fg="snow", font=(None, "16"), width=33).pack(side="right")
 
     frame_board = tk.Frame(root, width=300, height=200)
     frame_board.pack()
@@ -114,7 +155,7 @@ def main():
     frame_player1.pack()
     frame_player1.place(x=50, y=200)
     #stats =
-    tk.Label(frame_player1, text="Player 1 Stats", bg="gray25", fg="snow", font=(None, "25")).pack(fill="both")
+    tk.Label(frame_player1, text=p1_name +"\'s Stats", bg="gray25", fg="snow", font=(None, "25")).pack(fill="both")
     cellToChange = tk.Label(frame_player1, text="Cells to Change:  " + str(defaultCells), bg="gray50", fg="black", font=(None, "15"), width=19, height=3, anchor="s")
     cellToChange.pack()
     remaining = tk.Label(frame_player1, text="Remaining White: 100%", bg="gray50", fg="black", font=(None, "15"), width=19, height=3, anchor="s")
@@ -130,7 +171,7 @@ def main():
     frame_player2.pack()
     frame_player2.place(x=1400-280, y=200)
     #stats2 =
-    tk.Label(frame_player2, text="Player 2 Stats", bg="gray25", fg="snow", font=(None, "25")).pack(fill="both")
+    tk.Label(frame_player2, text=p2_name +"\'s Stats", bg="gray25", fg="snow", font=(None, "25")).pack(fill="both")
     cellToChange2 = tk.Label(frame_player2, text="Cells to Change:  " + str(defaultCells), bg="gray50", fg="black", font=(None, "15"), width=20, height=3, anchor="s")
     cellToChange2.pack()
     remaining2 = tk.Label(frame_player2, text="Remaining White: 100%", bg="gray50", fg="black", font=(None, "15"), width=20, height=3, anchor="s")
@@ -147,7 +188,7 @@ def main():
     frame_grid.place(x=304, y=260)
     canvas = tk.Canvas(frame_grid, width=366, height=366)
     canvas.pack()
-    p1Game = Game(canvas, root, p1Frame, default_speed, 1, 1, green[0], green[1], defaultCells)
+    p1Game = Game(canvas, root, p1Frame, default_speed, 1, 1, colors[p1_color][0], colors[p1_color][1], defaultCells)
     #create_grid()
     #scanvas.bind("<Button-1>", change_colour_on_click)
 
@@ -157,11 +198,10 @@ def main():
     canvas2 = tk.Canvas(frame_grid2, width=366, height=366)
     canvas2.pack()
 ###insert player 2 grid here
-    p2Game = Game(canvas2, root, p2Frame, default_speed, 2, 1, blue[0], blue[1], defaultCells)
+    p2Game = Game(canvas2, root, p2Frame, default_speed, 2, 1, colors[p2_color][0], colors[p2_color][1], defaultCells)
     p1Game.adversary = p2Game
     p2Game.adversary = p1Game
 ###insert player 2 grid button listener her
-
 
     root.mainloop()
 main()
